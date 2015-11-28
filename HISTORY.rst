ecf8cb83179be5993b70ea5eefbbecd0c41bbfe9
Nick Frost <nickfrostatx@gmail.com>
2015-11-28 03:44:01 -0600

    v0.1.2

--- HISTORY.rst
+++ HISTORY.rst
@@ -3,6 +3,11 @@
 History
 -------
 
+0.1.2 (2015-11-28)
+++++++++++++++++++
+
+- Use Travis for automated deployment to PyPI
+
 0.1.1 (2015-11-28)
 ++++++++++++++++++
 
--- walls.py
+++ walls.py
@@ -21,7 +21,7 @@ except ImportError:
 __author__ = 'Nick Frost'
 __copyright__ = 'Copyright 2015, Nicholas Frost'
 __license__ = 'MIT'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __email__ = 'nickfrostatx@gmail.com'
 
 
