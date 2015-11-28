e5f04280c8e2029c6afb6f5ece4c400bd394d4d2
Nick Frost <nickfrostatx@gmail.com>
2015-11-28 03:29:49 -0600

    v0.1.1

--- walls.py
+++ walls.py
@@ -21,7 +21,7 @@ except ImportError:
 __author__ = 'Nick Frost'
 __copyright__ = 'Copyright 2015, Nicholas Frost'
 __license__ = 'MIT'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __email__ = 'nickfrostatx@gmail.com'
 
 
